54060f6ccbf6257631949720b75fe2c98ce3ba03
yangjisu <yangjisue@naver.com>
2021-06-22 18:59:07 +0900

    댓글기능 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from. import views
 urlpatterns= [
 path('blog/', views.blog, name='blog'),
 path('post/<int:pk>/',views.post_detail,name='post_detail'),
 path('post/new/',views.post_new,name='post_new'),
 path('post/<int:pk>/edit/',views.post_edit,name='post_edit'),
+url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 path('',views.main,name='main'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import Post
 
 from django.shortcuts import render,get_object_or_404
-from .forms import PostForm
+from .forms import PostForm , CommentForm
 from django.shortcuts import redirect
 from django.core.paginator import Paginator
 # Create your views here.
@@ -48,3 +48,15 @@ def post_edit(request, pk):
 
 def main(request):
     return render(request, 'blog/main.html')
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
